f04b3f1eb9e7fe105f96b46dd5d57f95035f6a53
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-30 11:12:11 +0100

    add changes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,9 +5,10 @@ A module
 
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
-    """
-    obfuscated message method
-    """
-    pattern = fr'({separator.join(fields)})[^{separator}]*'
-    return re.sub(pattern, redaction, message)
+    # Create a regex pattern to match the specified fields and their values
+    pattern = fr'({"|".join(map(re.escape, fields))})=[^;]+'
+    
+    # Use re.sub to replace the matched fields with redaction
+    return re.sub(pattern, f'\\1={redaction}', message)

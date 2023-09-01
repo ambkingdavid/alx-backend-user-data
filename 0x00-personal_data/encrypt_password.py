1ca6f135990e500a2875d6cb1671cf19c4989f90
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-01 10:53:01 +0100

    add update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,15 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validate if a password matches a stored hashed passwd
+    """
+    try:
+        # Check if the provided password matches the hashed password
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    except ValueError:
+        # Handle invalid hashed_password gracefully
+        return False

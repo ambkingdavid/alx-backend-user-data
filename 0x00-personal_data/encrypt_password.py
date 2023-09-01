f28ca94f4100cf3e93cd807c29c201c3d091a6fb
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-01 10:49:36 +0100

    add changes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,10 +10,10 @@ def hash_password(password: str) -> bytes:
     """"
     password hashing
     """
-    #generate a random salt
+    # generate a random salt
     salt = bcrypt.gensalt()
 
-    #hash the password using the salt
+    # hash the password using the salt
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
     return hashed_password

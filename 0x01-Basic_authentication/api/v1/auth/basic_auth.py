c9a77b6593f44b0e37f0c1880a7b3befabb53f44
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-05 10:19:36 +0100

    add doc

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,6 +44,9 @@ class BasicAuth(Auth):
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
                                  ) -> (str, str):
+        """
+        extract email and password
+        """
         if decoded_base64_authorization_header is None:
             return ((None, None))
         if not isinstance(decoded_base64_authorization_header, str):

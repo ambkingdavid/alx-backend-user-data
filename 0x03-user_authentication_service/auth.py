91fffb89a9d199993bcdb1bedb7033b21d6094b6
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-12 13:53:42 +0100

    add changes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,7 +71,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            id = self._generate_uuid()
+            id = _generate_uuid()
             self._db.update_user(user.id, session_id=id)
             return id
         except NoResultFound:

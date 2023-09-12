f4125f9843007581ec556c27c95abfebdd318697
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-12 15:15:50 +0100

    add changes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -96,8 +96,5 @@ class Auth:
         """
         destroys a session
         """
-        try:
-            user = self._db.find_user_by(user_id=user_id)
-            self._db.update_user(user.id, session_id=None)
-        except NoResultFound:
-            pass
+        user = self._db.find_user_by(user_id=user_id)
+        self._db.update_user(user.id, session_id=None)
